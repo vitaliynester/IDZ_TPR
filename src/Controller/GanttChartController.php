8348b12de932f40f0482c47d3742062a93230ebd
Vitaly Posadnev <elemabor@gmail.com>
2021-06-02 11:14:17 +0300

    add: colorize chart from task status for detail team

--- src/Controller/GanttChartController.php
+++ src/Controller/GanttChartController.php
@@ -103,6 +103,7 @@ class GanttChartController extends AbstractController
             $data[$taskId]['start_date'] = $task->getStartDate();
             $data[$taskId]['end_date'] = $task->getEndDate();
             $data[$taskId]['team'] = $task->getWorkTeam() ? $task->getWorkTeam()->getId() : -1;
+            $data[$taskId]['color'] = $task->getStatus() == 1 ? '32CD32' : ($task->getStatus() == 2 ? 'FFFF00' : '8B0000');
         }
 
         return $this->render('gantt_chart/index.html.twig', [
